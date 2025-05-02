8b94b02e2523e604ab32ee3bcf2453b283dcb3d5
indeec <murathamatanov@gmail.com>
2025-05-02 19:23:48 +0300

    add fourth homework project

---
+++ Hw4/Hw4.test/Test1.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Hw4.test
+{
+    using Hw4;
+
+    [TestClass]
+    [DoNotParallelize]
+    public sealed class Test1
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void OptimumEmptyTest()
+        {
+            Routers.Optimum("../../../TestData/testEmpty.txt", "../../../TestData/testOutEmpty.txt");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void OptimumDisconnectedTest()
+        {
+            Routers.Optimum("../../../TestData/testDisconnected.txt", "../../../TestData/testOutDisconnected.txt");
+        }
+
+        [TestMethod]
+        public void OptimumTest()
+        {
+            Routers.Optimum("../../../TestData/test1.txt", "../../../TestData/testOut1.txt");
+            var sr = new StreamReader("../../../TestData/testOut1.txt");
+            Assert.AreEqual(sr.ReadLine(), "1: 2 (100), 5 (100)");
+            Assert.AreEqual(sr.ReadLine(), "2: 3 (100)");
+            Assert.AreEqual(sr.ReadLine(), "3: 4 (100)");
+
+        }
+    }
+}
---
+++ Hw4/Hw4/Routers.cs
@@ -0,0 +1,138 @@
+﻿
+using System.Xml.Serialization;
+
+namespace Hw4
+{
+    public class Routers
+    {
+        public static void Optimum(string pathInput, string pathOutput)
+        {
+            var graph = new Dictionary<int, Dictionary<int, int>>();
+            var sr = new StreamReader(pathInput);
+            string current = sr.ReadLine()!;
+            if (current == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            while (current != null)
+            {
+                string[] fragmentized = current.Split(" ").ToArray();
+                int main = Convert.ToInt32(fragmentized[0][0..(fragmentized[0].Length - 1)]);
+                if (!graph.ContainsKey(main))
+                {
+                    graph.Add(main, new Dictionary<int, int>());
+                }
+
+                int currentVertex;
+                int currentWeight;
+                for (int i = 1; i < fragmentized.Length - 2; i += 2)
+                {
+                    currentVertex = Convert.ToInt32(fragmentized[i]);
+                    currentWeight = Convert.ToInt32(fragmentized[i + 1][1..(fragmentized[i + 1].Length - 2)]);
+                    if (!graph.ContainsKey(currentVertex))
+                    {
+                        graph.Add(currentVertex, new Dictionary<int, int>());
+                    }
+
+                    graph[main].Add(currentVertex, currentWeight);
+                }
+
+                currentVertex = Convert.ToInt32(fragmentized[fragmentized.Length - 2]);
+                currentWeight = Convert.ToInt32(fragmentized[fragmentized.Length - 1][1..(fragmentized[fragmentized.Length - 1].Length - 1)]);
+
+                if (!graph.ContainsKey(currentVertex))
+                {
+                    graph.Add(currentVertex, new Dictionary<int, int>());
+                }
+
+                graph[main].Add(currentVertex, currentWeight);
+                current = sr.ReadLine()!;
+            }
+
+            sr.Close();
+
+            if (!IsConnected(graph))
+            {
+                throw new ArgumentException("disconnected graph");
+            }
+
+            var maxGraph = new Dictionary<int, Dictionary<int, int>>();
+            bool[] used = new bool[graph.Keys.Max() + 1];
+            var queue = new PriorityQueue<(int, int, int), int>(Comparer<int>.Create((x, y) => y.CompareTo(x))); // previous vertex , next vertex , weight
+            queue.Enqueue((graph.Keys.First(), graph.Keys.First(), 0), 0); // we well make one loop for root vertex and delete it later
+            while (queue.Count != 0)
+            {
+                var currentEdge = queue.Dequeue();
+                int previousVertex = currentEdge.Item1;
+                int nextVertex = currentEdge.Item2;
+                int weight = currentEdge.Item3;
+
+                if (used[nextVertex])
+                {
+                    continue;
+                }
+
+                used[nextVertex] = true;
+                maxGraph.Add(nextVertex, new Dictionary<int, int>());
+                maxGraph[previousVertex].Add(nextVertex, weight);
+
+                foreach (var vertexWithWeight in graph[nextVertex])
+                {
+                    queue.Enqueue((nextVertex, vertexWithWeight.Key, vertexWithWeight.Value), vertexWithWeight.Value);
+                }
+            }
+
+            maxGraph[graph.Keys.First()].Remove(graph.Keys.First());
+
+            var sw = new StreamWriter(pathOutput);
+
+            foreach (var vertex in maxGraph)
+            {
+                if (vertex.Value.Count > 0)
+                {
+                    sw.Write($"{vertex.Key}:");
+
+                    string textWithLast = string.Empty;
+
+                    foreach (var nextVertex in vertex.Value)
+                    {
+                        textWithLast += $" {nextVertex.Key} ({nextVertex.Value}),";
+                    }
+
+                    sw.WriteLine(textWithLast[0..(textWithLast.Length - 1)]); // because we have extra ',' on the end
+                }
+            }
+
+            sw.Close();
+        }
+
+        private static bool IsConnected(Dictionary<int, Dictionary<int, int>> graph)
+        {
+            bool[] used = new bool[graph.Keys.Max() + 1];
+            int currentVertex = graph.Keys.First();
+            while (!used[currentVertex])
+            {
+                used[currentVertex] = true;
+                foreach (var nextVertex in graph[currentVertex])
+                {
+                    if (!used[nextVertex.Key])
+                    {
+                        currentVertex = nextVertex.Key;
+                        break;
+                    }
+                }
+            }
+
+            foreach (var key in graph.Keys)
+            {
+                if (!used[key])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
