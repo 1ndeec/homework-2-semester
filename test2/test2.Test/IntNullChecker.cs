a314e3b5387c37df4c5ad6d909fb5a402d860fdd
murathamatanov <murathamatanov@gmail.com>
2025-05-23 05:20:00 -0700

    add second test

---
+++ test2/test2.Test/IntNullChecker.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Murat Khamatyanov. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Check.Test;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test2;
+
+/// <summary>
+/// Provides a method a check whether is integer is null (zero).
+/// </summary>
+public class IntNullChecker : INullChecker<int>
+{
+    /// <summary>
+    /// Checks whether is integer is null (zero).
+    /// </summary>
+    /// <param name="item"> Item to check. </param>
+    /// <returns> true if given integer is zero, false if not. </returns>
+    public bool IsNull(int item) => item == 0;
+}
---
+++ test2/test2/INullChecker.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Murat Khamatyanov. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Test2;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Defines a method to determine if an item is considered null for a specific type.
+/// </summary>
+/// <typeparam name="T">The type of items to check. </typeparam>
+public interface INullChecker<in T>
+{
+    /// <summary>
+    /// Returns if given element is null.
+    /// </summary>
+    /// <param name="item"> Element to check whether its null. </param>
+    /// <returns> True if null, false if not. </returns>
+    bool IsNull(T item);
+}
