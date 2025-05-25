b3e1dd32df813ecb14e959caf2169238e39de954
indeec <murathamatanov@gmail.com>
2025-05-25 21:16:39 +0300

    add fifth homework

---
+++ Hw5/Hw5/AddNode.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Murat Khamatyanov. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Hw5;
+
+/// <summary>
+/// Represents a node that stores a addition operation.
+/// </summary>
+/// <param name="left">First operand.</param>
+/// <param name="right">Second operand.</param>
+public class AddNode(Node left, Node right) : OperatorNode(left, right)
+{
+    /// <summary>
+    /// Computes the result of the addition operation.
+    /// </summary>
+    /// <returns> Node`s addition operation result. </returns>
+    public override int Compute()
+    {
+        return this.Left.Compute() + this.Right.Compute();
+    }
+
+    /// <summary>
+    /// Returns a string representation of addition operation stored in node.
+    /// </summary>
+    /// <returns>A string representation of addition operation stored in node.</returns>
+    public override string Print()
+    {
+        return $"( + {this.Left.Print()} {this.Right.Print()} )";
+    }
+}
---
+++ Hw5/Hw5/OperatorNode.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Murat Khamatyanov. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Hw5;
+
+/// <summary>
+/// Represents a node that stores an operation.
+/// </summary>
+/// <param name="left">First operand.</param>
+/// <param name="right">Second operand.</param>
+public abstract class OperatorNode(Node left, Node right) : Node
+{
+    private Node left = left;
+    private Node right = right;
+
+    /// <summary>
+    /// Gets first operand.
+    /// </summary>
+    protected Node Left => this.left;
+
+    /// <summary>
+    /// Gets second operand.
+    /// </summary>
+    protected Node Right => this.right;
+}
